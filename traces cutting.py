2210d4a73231cb3f72ca510026a9ad93dc5a73a3
jason <jason032089@gmail.com>
2022-11-04 16:57:02 +0800

    try using h5py to create dataset

--- traces cutting.py
+++ traces cutting.py
@@ -2,13 +2,19 @@ from read_tsmip import *
 import os
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from datetime import timedelta
+import h5py
 
 Afile_path="data/Afile"
 catalog=pd.read_csv(f"{Afile_path}/final catalog.csv")
-traces=pd.read_csv(f"{Afile_path}/2012-2020 traces with picking and label.csv")
+traces=pd.read_csv(f"{Afile_path}/2012-2020 traces with picking and label_new.csv")
+station_info=pd.read_csv(f"data/TSMIPstations.csv")
+station_code=station_info["station"].str.extract(r'(.*?)[(]')
+location_code=station_info["station"].str.extract(r'[(](.*?)[)]')
+station_info.insert(1,"station_code",station_code.values)
+station_info.insert(2,"location_code",location_code.values)
+station_info.drop(["station"],axis=1,inplace=True)
+station_info.to_csv(f"data/TSMIPstations_new.csv",index=False)
+
 traces.loc[traces.index,"p_picks (sec)"]=pd.to_timedelta(traces["p_picks (sec)"],unit="sec")
 traces.loc[traces.index,"start_time"]=pd.to_datetime(traces['start_time'], format='%Y%m%d%H%M%S')
 
@@ -16,102 +22,26 @@ eq_id=2561
 eq_id=7613
 eq_id=4682
 eq_id=423
-before_p_sec=5
-trace_length_sec=30
 
-traces_filter=(traces["EQ_ID"]==eq_id)
-tmp_traces=traces[traces_filter]
-
-# tmp_traces["p_picks (sec)"]=pd.to_timedelta(tmp_traces["p_picks (sec)"],unit="sec")
-# tmp_traces['start_time'] = pd.to_datetime(tmp_traces['start_time'], format='%Y%m%d%H%M%S')
-sorted_indices = (tmp_traces["start_time"] + tmp_traces["p_picks (sec)"])\
-                    .sort_values().index
-tmp_traces=tmp_traces.loc[sorted_indices, :].reset_index(drop=True)
-year=tmp_traces["year"][0]
-month=tmp_traces["month"][0]
-path=f"data/waveform/{year}/{month}"
-file_name=tmp_traces["file_name"][0]
-
-if len(str(month))<2:
-    month="0"+str(month)
-path=f"data/waveform/{year}/{month}"
-file_name=file_name.strip()
-stream=read_tsmip(f"{path}/{file_name}.txt")
-sampling_rate=stream[0].stats["sampling_rate"]
-trace=np.transpose(np.array(stream))/100 #cm/s^2 to m/s^2
-
-trace_length_point=int(trace_length_sec*sampling_rate)
-first_start_cut_point=int((np.round(tmp_traces["p_picks (sec)"][0].total_seconds(),2)-before_p_sec)*sampling_rate)
-if first_start_cut_point<0:
-    first_start_cut_point=0
-if first_start_cut_point+trace_length_point>len(trace): #zero padding
-    init_trace=trace[first_start_cut_point:,:]
-    init_trace=np.pad(init_trace,((0,trace_length_point-len(init_trace)),(0,0)),"constant")
-else:
-    init_trace=trace[first_start_cut_point:first_start_cut_point+trace_length_point,:]
-
-p_picks_point=int(np.round(tmp_traces["p_picks (sec)"][0].total_seconds()*sampling_rate,0)-first_start_cut_point)
-pga_time=int(tmp_traces["pga_time"][0]-first_start_cut_point)
-
-if len(tmp_traces)>1:
-    fig,ax=plt.subplots(len(tmp_traces),1,figsize=(14,7))
-    ax[0].plot(init_trace)
-    ymin,ymax=ax[0].get_ylim()
-    ax[0].vlines(p_picks_point,ymin,ymax,"r")
-    ax[0].set_title(f"EQ ID: {eq_id}",fontsize=20)
-    ax[0].set_yticks([])
-
-else:
-    fig,ax=plt.subplots(figsize=(14,7))
-    ax.plot(init_trace)
-    ymin,ymax=ax.get_ylim()
-    ax.vlines(p_picks_point,ymin,ymax,"r")
-    ax.scatter(pga_time,tmp_traces["pga"][0])
-
-if len(tmp_traces)>1:
-    print("more than  1 traces")
-    for i in range(1,len(tmp_traces)):
-
-        year=tmp_traces["year"][i]
-        month=tmp_traces["month"][i]
-        path=f"data/waveform/{year}/{month}"
-        file_name=tmp_traces["file_name"][i]
-
-        if len(str(month))<2:
-            month="0"+str(month)
-        path=f"data/waveform/{year}/{month}"
-        file_name=file_name.strip()
-        stream=read_tsmip(f"{path}/{file_name}.txt")
-        sampling_rate=stream[0].stats["sampling_rate"]
-        trace=np.transpose(np.array(stream))/100 #cm/s^2 to m/s^2
-        window_cut_time=(tmp_traces["start_time"][0]-tmp_traces["start_time"][i]).total_seconds()+\
-                        (first_start_cut_point/sampling_rate)
-        start_cut_point=int(window_cut_time*sampling_rate)
-        if window_cut_time<0:
-            print("pad at the beginning")
-            end_cut_time=trace_length_point+start_cut_point
-            if end_cut_time<=0:
-                cutting_trace=np.zeros([trace_length_point,3])
-            else:
-                cutting_trace=trace[:end_cut_time,:]
-                cutting_trace=np.pad(cutting_trace,((abs(start_cut_point),0),(0,0)),"constant")
-        else:
-            print("no pad")
-            cutting_trace=trace[start_cut_point:start_cut_point+trace_length_point,:]
-        p_picks_point=int(np.round(tmp_traces["p_picks (sec)"][i].total_seconds()*sampling_rate,0)-start_cut_point)
-        pga_time=int(tmp_traces["pga_time"][i]-start_cut_point)
-
-        print(p_picks_point)
-        ax[i].plot(cutting_trace)
-        ax[i].set_yticks([])
-        ymin,ymax=ax[i].get_ylim()
-        if p_picks_point<trace_length_point:
-            ax[i].vlines(p_picks_point,ymin,ymax,"r")
+output="data/TSMIP.hdf5"
+with h5py.File(output, 'w') as file:
+        data = file.create_group('data')
+        meta = file.create_group('metadata')
+        for eq_id in catalog["EQ_ID"][:20]:
+            tmp_traces,traces_info=cut_traces(traces,eq_id)
+            fig=plot_cutting_event(tmp_traces,traces_info,close_fig=True)
+            start_time_str_arr = np.array(traces_info["start_time"],dtype='S30')
+            event = data.create_group(f"{eq_id}")
+            event.create_dataset("traces", data=traces_info["traces"],dtype=np.float64)
+            event.create_dataset("p_picks", data=traces_info["p_picks"],dtype=np.int64)
+            event.create_dataset("pga",data=traces_info["pga"],dtype=np.float64)
+            event.create_dataset("pgv",data=traces_info["pgv"],dtype=np.float64)
+            event.create_dataset("start_time", data=start_time_str_arr,maxshape=(None),chunks=True)
+            event.create_dataset("pga_time", data=traces_info["pga_time"],dtype=np.int64)
+            event.create_dataset("pgv_time", data=traces_info["pgv_time"],dtype=np.int64)
+            # fig.savefig(f"data/cutting waveform image/{eq_id}.png")
 
 
-
-# fig,ax=plt.subplots(2,1,figsize=(14,7))
-# ax[0].plot(init_trace)
-# ax[1].plot(cutting_trace)
-
+catalog.to_hdf(output, key='metadata/event_metadata', mode='a', format='table')
+traces.to_hdf(output, key='metadata/traces_metadata', mode='a', format='table')
 
