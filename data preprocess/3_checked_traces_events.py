52b5b7bab6100878407c279de75e80b78fdc70cd
jason <jason032089@gmail.com>
2023-06-01 15:42:14 +0800

    refresh comment

--- data preprocess/3_checked_traces_events.py
+++ data preprocess/3_checked_traces_events.py
@@ -15,7 +15,7 @@ ok_traces=traces[y_filter]
 
 
 
-#本來有4級的traces但是壞掉了 需要把其他traces替除
+#本來有4級的traces且整個事件只有1個但是壞掉了 需要把其他traces替除
 intensity_filter=(ok_traces["intensity"]>=4)
 include_intensity_4=ok_traces[intensity_filter]["EQ_ID"].unique().tolist()
 ok_traces_filter=(ok_traces["EQ_ID"].isin(include_intensity_4))
@@ -29,10 +29,11 @@ ax.hist(traces["intensity"],bins=16,edgecolor="gray")
 ax.hist(ok_traces["intensity"],bins=16,edgecolor="gray")
 plt.yscale("log")
 
-#把events也替除
+#上述問題在events也替除
 ok_event_filter=(events["EQ_ID"].isin(include_intensity_4))
 ok_events=events[ok_event_filter]
 ok_events.to_csv("2009_2019_ok_events.csv",index=False)
+
 fig,ax=plt.subplots()
 ax.hist(events["magnitude"],bins=28,edgecolor="gray")
 ax.hist(ok_events["magnitude"],bins=28,edgecolor="gray")
