4d74a7b8bc7ae63c5f9fc759360e74d708e374f1
jason <jason032089@gmail.com>
2023-09-05 16:11:06 +0800

    refresh

--- intensity_map.py
+++ intensity_map.py
@@ -1,7 +1,7 @@
 import h5py
 import numpy as np
 import pandas as pd
-
+import os
 from plot_predict_map import (
     correct_warning_with_epidist,
     plot_intensity_map,
@@ -19,7 +19,10 @@ if label_type == "pgv":
     label_threshold = np.log10(0.15)
     intensity = "V"
 
-path = "./predict/station_blind_noVs30_bias2closed_station_2016"
+path = "./predict/station_blind_Vs30_bias2closed_station_2016"
+output_path=f"{path}/mag bigger 5.5 predict"
+if not os.path.isdir(output_path):
+    os.mkdir(output_path)
 Afile_path = "data preprocess/events_traces_catalog"
 
 catalog = pd.read_csv(f"{Afile_path}/1999_2019_final_catalog.csv")
@@ -27,7 +30,7 @@ traces_info = pd.read_csv(
     f"{Afile_path}/2009_2019_picked_traces_p_arrival_abstime_labeled_nostaoverlap.csv"
 )
 prediction_with_info = pd.read_csv(
-    f"predict/station_blind_noVs30_bias2closed_station_2016/{mask_after_sec} sec ensemble 510 with all info.csv"
+    f"{path}/{mask_after_sec} sec model11 with all info.csv"
 )
 
 for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
@@ -51,7 +54,7 @@ for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
         title=f"EQID: {EQ_ID}, mag: {event['magnitude'].values[0]}, {mask_after_sec} sec PGA intensity Map",
     )
     # fig.savefig(
-    #     f"{path}/mag bigger 5.5 predict from model 510/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec}sec PGA intensity Map.png", dpi=450, bbox_inches="tight"
+    #     f"{output_path}/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec}sec PGA intensity Map.png", dpi=450, bbox_inches="tight"
     # )
     fig, ax = true_predicted(
         y_true=event_prediction["answer"],
@@ -64,7 +67,7 @@ for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
         title=f"EQID: {EQ_ID}, mag: {event['magnitude'].values[0]}, {mask_after_sec} sec true and predict",
     )
     # fig.savefig(
-    #     f"{path}/mag bigger 5.5 predict from model 510/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec}sec true predict plot.png", dpi=450, bbox_inches="tight"
+    #     f"{output_path}/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec}sec true predict plot.png", dpi=450, bbox_inches="tight"
     # )
     try:
         fig, ax = warning_map(
@@ -77,7 +80,7 @@ for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
             label_threshold=label_threshold,
         )
 
-        # fig.savefig(f"{path}/meinong earthquake/{mask_after_sec} sec warning map.png",
+        # fig.savefig(f"{output_path}/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec} sec warning map.png",
         #             dpi=300)
         fig, ax = correct_warning_with_epidist(
             event_prediction=event_prediction,
@@ -85,7 +88,7 @@ for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
             label_type=label_type,
             mask_after_sec=mask_after_sec,
         )
-        # fig.savefig(f"{path}/meinong earthquake/{mask_after_sec} sec epidist vs time.png",
+        # fig.savefig(f"{output_path}/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec} sec epidist vs time.png",
         #             dpi=300)
         fig, ax = warning_time_hist(
             event_prediction,
@@ -98,12 +101,14 @@ for EQ_ID in catalog.query("year==2016 & magnitude>=5.5")["EQ_ID"]:
             bins=14,
         )
         # fig.savefig(
-        #     f"{path}/{mask_after_sec} sec warning stations hist.png",
+        #     f"{output_path}/{EQ_ID}_mag_{event['magnitude'].values[0]}_{mask_after_sec} sec warning stations hist.png",
         #     dpi=300,
         #     bbox_inches="tight",
         # )
-    except:
+    except Exception as e:
         print(EQ_ID)
+        continue
+
 
 
 ############################################################## M3.5 + M5.5
